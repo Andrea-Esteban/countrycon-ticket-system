94440548b4006896a9413477bebe88225bb49ec0
Andrea-Esteban <67924226+Andrea-Esteban@users.noreply.github.com>
2026-08-23 15:02:08 -0500

    Configure production deployment

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 from dotenv import load_dotenv
 import dj_database_url
 load_dotenv()
